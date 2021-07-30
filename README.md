b3c0680f0c859f56b173e503275c2533030e234b
YoonHwe <dbsghl676@gmail.com>
2021-07-30 15:33:24 +0900

    windowResize (with innerWidth)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # vanilla-js
  바닐라 JS 기초 과제
  HW 1 => addEventListener
+ HW 2 => windowResize (with innerWidth)
--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-// <⚠️ DONT DELETE THIS ⚠️>
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-// <⚠️ /DONT DELETE THIS ⚠️>
-
-/*
-✅ The text of the title should change when the mouse is on top of it.
-✅ The text of the title should change when the mouse is leaves it.
-✅ When the window is resized the title should change.
-✅ On right click the title should also change.
-✅ The colors of the title should come from a color from the colors array.
-✅ DO NOT CHANGE .css, or .html files.
-✅ ALL function handlers should be INSIDE of "superEventHandler"
-*/
-
-const superEventHandler = {
-  func1: function mouseOn() {
-    hello.style.color = colors[0];
-    hello.innerText = "The mouse is here!";
-  },
-  func2: function mouseDown() {
-    hello.style.color = colors[1];
-    hello.innerText = "The mouse is gone!";
-  },
-  func3: function windowResize() {
-    hello.style.color = colors[2];
-    hello.innerText = "You just resized!";
-  },
-  func4: function mouseClick() {
-    hello.style.color = colors[3];
-    hello.innerText = "That was right click!";
+const html = document.querySelector("html");
+const hello = document.querySelector("h1");
+hello.style.color = "white";
+function windowSizeChange() {
+  if (window.innerWidth < 600) {
+    html.style.backgroundColor = "blue";
+  } else if (window.innerWidth >= 600 && window.innerWidth < 900) {
+    html.style.backgroundColor = "purple";
+  } else {
+    html.style.backgroundColor = "yellow";
   }
-};
-
-const hello = document.querySelector("h2");
-
-hello.addEventListener("mouseover", superEventHandler.func1);
-hello.addEventListener("mouseleave", superEventHandler.func2);
-window.addEventListener("resize", superEventHandler.func3);
-hello.addEventListener("mousedown", superEventHandler.func4);
+}
+window.addEventListener("resize", windowSizeChange);
